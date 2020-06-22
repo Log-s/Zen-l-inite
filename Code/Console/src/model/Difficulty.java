032eebead97cf31aef64cf57d2545d865c336f69
Léo Desmonts <leo951206@gmail.com>
2020-06-22 08:34:39 +0200

    Update Difficulty.java

--- Code/Console/src/model/Difficulty.java
+++ Code/Console/src/model/Difficulty.java
@@ -2,7 +2,7 @@ package model;
 
 /**
  * Enumeration
- * Possible difficultie levels for Automated player
+ * Possible difficultie levels for Automated player (only RANDOM is implemented)
  * 
  * @author Léo DESMONTS - INFO VANNES - 2020
  * @version 1.0
